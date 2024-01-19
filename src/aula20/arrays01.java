876dad9883a140a6413428b5d9884b5c7ef7ca09
Willianr7 <Rafael.net@hotmail.com.br>
2024-01-19 16:56:00 -0300

    subindo os exercicios da aula 20

---
+++ src/aula20/arrays01.java
@@ -0,0 +1,46 @@
+package aula20;
+
+import java.util.Random;
+
+public class arrays01 {
+
+	public static void main(String[] args) {
+		
+		
+		int[][] numerosAleatorios = new int[4][4];
+
+        Random random = new Random();
+
+        for (int i = 0; i < numerosAleatorios.length; i++) {
+            for (int j = 0; j < numerosAleatorios[i].length; j++) {
+                numerosAleatorios[i][j] = random.nextInt(9);
+            }
+        }
+
+        int maior = Integer.MIN_VALUE; 
+        int linha = 0, coluna = 0;
+
+        for (int i = 0; i < numerosAleatorios.length; i++) {
+            for (int j = 0; j < numerosAleatorios[i].length; j++) {
+                if (numerosAleatorios[i][j] > maior) {
+                    maior = numerosAleatorios[i][j];
+                    linha = i;
+                    coluna = j;
+                }
+            }
+        }
+
+        for (int i = 0; i < numerosAleatorios.length; i++) {
+            for (int j = 0; j < numerosAleatorios[i].length; j++) {
+                System.out.print(numerosAleatorios[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("Maior valor = " + maior);
+        System.out.println("Linha = " + linha);
+        System.out.println("Coluna = " + coluna);
+
+	}
+
+}
